086671c0dfb1bf3eceac6c2e00a2697c9b68cd37
naoko kasahara <kassa0918@gmail.com>
2021-05-09 22:23:57 +0900

    kadai1_文章投稿機能、全体タイムラインに文章内容及び文章作成日時表示

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,22 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(permitted_parameter)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render :new
+    end
+  end
+  private
+  def permitted_parameter
+    params.require(:blog).permit(:content)
   end
 end
